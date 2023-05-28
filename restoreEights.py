2581cd7a6ab561f819c3a024d170f262601a1d42
Poogity <108656825+Poogity@users.noreply.github.com>
2023-05-28 10:48:40 +0300

    Add files via upload

--- restoreEights.py
+++ restoreEights.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 inputSize = 10
-N=500
-
+N = 400
+n_iters = 1000
 def ReLU(W1):
     Z1 = np.empty((128,1))
     for i in range(128):      
@@ -22,22 +22,25 @@ def forwProp(Z):
     return X,W1,W2
 
 def gradDescent(Z,P):
-    lamda = 0.01
+    lamda = 0.0001
     if P.all() ==0: lamda=1
-    lr = 0.01
-    Zimg,W1,W2 = forwProp(Z)
-    U2 = np.dot(2*T,1/((TX-Xn)+c))
-    dSigmoid = ((-np.exp(W2))/(1+np.exp(W2)))**2
+    lr = 5
+    X,W1,W2 = forwProp(Z)
+    TX = np.dot(T,X)
+    U2 = np.dot(2*T.T/np.linalg.norm((TX-Xn+c)**2),TX-Xn)
+    dSigmoid = -(np.exp(W2))/((1+np.exp(W2)))**2
     V2= (U2*dSigmoid)
+
     U1 = np.dot(A2.T,V2)
-    V1 = U1 *(W1 > 0)*1  
+    V1 = U1 *(W1 > 0)*1 
     U0 = np.dot(A1.T,V1)
     gradJ = N*U0+2*Z
-    J = np.mean(N*np.log(abs(TX-Xn+c)**2)+abs(Zimg)**2)
+    J = (N*np.log(np.linalg.norm(TX-Xn+c)**2)+np.linalg.norm((Z)**2))
+    #print(f'cost value = {J:.4f}')
     P = (1-lamda)*P + lamda*(gradJ**2)
     
     Z = Z - lr*gradJ/(c+P)
-    return Z,P,Zimg
+    return Z,P,J
 
 
 c= 10**(-5)
@@ -54,47 +57,54 @@ B2 = mat["B_2"]
 #FORWARD PROPEGATION
 #Input is 10 elements of independant Gaussian random variables
 Z = np.empty((inputSize,1))
-for i in range(inputSize):
-    Z[i] = np.random.normal(0,1)
-X,_,_ = forwProp(Z) #Generative Model
+
 
 mat = scipy.io.loadmat('data22.mat')
 XN = mat["X_n"]
 XI = mat["X_i"]
 
 
-T = np.eye(N,784,order='C')
-T=np.vstack((T,np.zeros((784-N,784))))
-
+T = np.identity(N)
+T=np.hstack((T,np.zeros((N,784-N))))
+cAA = np.empty((4,n_iters))
 for k in range(4):
+    minJ = float("inf")
     Xn = np.dot(T,XN[:,k])
-    Xn=np.reshape(Xn,(784,1))
-    TX = np.dot(T,X)
+    Xn=np.reshape(Xn,(N,1))
     
     #initialize Z
     Z = np.empty((inputSize,1))
     for i in range(inputSize):
         Z[i] = np.random.normal(0,1)
     
-    P= np.zeros((10,1))    
-    for i in range(20):
-        Z,P,Zimg=gradDescent(Z,P)
+    P= np.zeros((10,1))
     
+    costArray = np.empty(n_iters)
+    for i in range(n_iters):
+        Z,P,J=gradDescent(Z,P)
+        costArray[i] = J
+        if(J<minJ): bestZ=Z
+    R,_,_ = forwProp(bestZ)
+    R = np.reshape(R, (28,28), order='F')
+    cAA[k] = costArray
     
-    #Z,_,_ = forwProp(Z)
-    Z = np.reshape(Zimg, (28,28), order='F')
-    
-    
-    
+    Xn = np.append(Xn,np.zeros(784-N))
     Xn=np.reshape(Xn,(28,28),order='F' )
     Xi=np.reshape(XI[:,k],(28,28),order='F')
     plt.subplot(4,3,3*k+1),plt.imshow(Xi, cmap = 'gray')
     plt.title('Original'), plt.xticks([]), plt.yticks([])
     plt.subplot(4,3,3*k+2),plt.imshow(Xn, cmap = 'gray')
     plt.title('Noisy'), plt.xticks([]), plt.yticks([])
-    plt.subplot(4,3,3*k+3),plt.imshow(Z, cmap = 'gray')
+    plt.subplot(4,3,3*k+3),plt.imshow(R, cmap = 'gray')
     plt.title('Restored'), plt.xticks([]), plt.yticks([])
-
 plt.show()
-    #plt.imshow(TX,cmap='gray')
-    #plt.xticks([]), plt.yticks([])
+
+xaxis = np.linspace(1, n_iters,n_iters)
+for i in range(4):  
+    plt.plot(xaxis,cAA[i])   
+
+
+
+
+#plt.imshow(TX,cmap='gray')
+#plt.xticks([]), plt.yticks([])
